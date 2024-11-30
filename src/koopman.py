3b6d0a890a17cd4cf47a9c5b0ff18bd89d4d50b0
juannat7 <nathanieljuan@gmail.com>
2024-11-30 14:38:48 -0500

    updates with longer dynamics

--- src/koopman.py
+++ src/koopman.py
@@ -1,7 +1,7 @@
 import jax
 import jax.numpy as jnp
 
-def random_fourier_features(X, M=500, key=jax.random.PRNGKey(42)):
+def random_fourier_features(X, M=500, key=jax.random.PRNGKey(42), identity=False):
     """
     Generate Random Fourier Features for input data X 
         Reference: Equation 10
@@ -12,21 +12,24 @@ def random_fourier_features(X, M=500, key=jax.random.PRNGKey(42)):
         key (jax.random.PRNGKey): Random seed.
 
     Returns:
-        jax.numpy.ndarray: Transformed data of shape (n_samples, M).
+        jax.numpy.ndarray: Transformed data of shape (M, D).
     """
+    if identity:
+        return X
+        
     N, D = X.shape
     key_W, key_b = jax.random.split(key)
 
-    W = jax.random.normal(key_W, shape=(N, M)) # Random projection matrix
+    W = jax.random.normal(key_W, shape=(N, M))  # Random projection matrix
     b = jax.random.uniform(key_b, shape=(M, D)) # Random biases
 
     # Compute RFF
-    return jnp.cos(jnp.dot(W.T, X) + b)
+    return jnp.sqrt(2 / M) * jnp.cos(jnp.dot(W.T, X) + b)
 
 
 def dynamic_mode_decomposition(Psi, Psi_t):
     """
-    Perform Dynamic Mode Decomposition (DMD) in the original space.
+    Perform Dynamic Mode Decomposition (DMD).
 
     Parameters:
         Psi: Observables at current time.
@@ -44,6 +47,40 @@ def dynamic_mode_decomposition(Psi, Psi_t):
     return K_t
 
 
+def compute_transforms(cause, effect, t=1):
+    """Compute cause-effect transforms"""
+    N, D = cause.shape
+    
+    effect_cause = jnp.concatenate([effect, cause], axis=0)
+
+    # Generate the omega and its shifts
+    omega_E, omega_Et = effect[:, :-t], effect[:, t:]
+    omega_EC, omega_ECt = effect_cause[:, :-t], effect_cause[:, t:]
+
+    # Compute their DMD transforms
+    psi_E, psi_Et = random_fourier_features(omega_E), random_fourier_features(omega_Et)
+    psi_EC, psi_ECt = random_fourier_features(omega_EC), random_fourier_features(omega_ECt)
+
+    return omega_E, omega_Et, omega_EC, omega_ECt, psi_E, psi_Et, psi_EC, psi_ECt
+
+
+
+def compute_K(omega_E, psi_E, omega_EC, psi_EC, omega_Et):
+    """Compute approximation to Koopman operator with DMD algorithm"""
+
+    # Compute marginal model
+    K_marginal = dynamic_mode_decomposition(
+        jnp.concatenate([omega_E, psi_E], axis=0), omega_Et
+    )
+
+    # Compute joint model
+    K_joint = dynamic_mode_decomposition(
+        jnp.concatenate([omega_E, psi_EC], axis=0), omega_Et
+    )
+
+    return K_marginal, K_joint
+
+
 def compute_causal_loss(cause, effect, t=1):
     """
     Compute causal loss for both causal and non-causal directions.
@@ -51,69 +88,30 @@ def compute_causal_loss(cause, effect, t=1):
     Parameters:
         cause: Causing data states.
         effect: Effected data states.
+        t: Time shifts.
 
     Returns:
         causal_model_error: Causal direction error.
-        non_causal_model_error: Non-causal direction error.
-        omega_marginal_causal, omega_joint_causal: Forecasts in causal direction.
-        omega_marginal_non_causal, omega_joint_non_causal: Forecasts in non-causal direction.
+        omega_marginal: Marginal forecasts.
+        omega_joint: Joint forecasts.
     """
-    N, D = cause.shape
-    cause_effect = jnp.concatenate([cause, effect], axis=0)
+    # Compute transforms
+    omega_E, omega_Et, omega_EC, omega_ECt, psi_E, psi_Et, psi_EC, psi_ECt = compute_transforms(cause, effect, t)
 
-    # Generate the omega and its shifts
-    omega_C, omega_C_t = cause[:, :-t], cause[:, t:]
-    omega_E, omega_E_t = effect[:, :-t], effect[:, t:]
-    omega_CE, omega_CE_t = cause_effect[:, :-t], cause_effect[:, t:]
-
-    # Compute their DMD transforms
-    psi_C, psi_C_t = random_fourier_features(omega_C), random_fourier_features(omega_C_t)
-    psi_E, psi_E_t = random_fourier_features(omega_E), random_fourier_features(omega_E_t)
-    psi_CE, psi_CE_t = random_fourier_features(omega_CE), random_fourier_features(omega_CE_t)
-
-    # Step 1: Causal Direction (C -> E)
-
-    ## Compute K
-    K_marginal_causal = dynamic_mode_decomposition(
-        jnp.concatenate([omega_E, psi_E], axis=0), omega_E_t
-    )
-    K_joint_causal = dynamic_mode_decomposition(
-        jnp.concatenate([omega_E, psi_CE], axis=0), omega_CE_t
-    )
-
-    ## Compute marginal / joint
-    omega_marginal_causal = K_marginal_causal @ jnp.concatenate([omega_E, psi_E], axis=0)
-    omega_joint_causal = K_joint_causal @ jnp.concatenate([omega_E, psi_CE], axis=0)
-
-    ## Compute errors
-    causal_err_marginal = jnp.square(omega_marginal_causal - omega_E_t).mean()
-    causal_err_joint = jnp.square(omega_joint_causal[:N] - omega_E_t).mean()
-    causal_model_error = causal_err_joint - causal_err_marginal
-
-    # Step 2: Non-Causal Direction (E -> C)
-
-    ## Compute K
-    K_marginal_non_causal = dynamic_mode_decomposition(
-        jnp.concatenate([omega_C, psi_C], axis=0), omega_C_t
-    )
-    K_joint_non_causal = dynamic_mode_decomposition(
-        jnp.concatenate([omega_C, psi_CE], axis=0), omega_CE_t
-    )
-
-    ## Compute marginal / joint
-    omega_marginal_non_causal = K_marginal_non_causal @ jnp.concatenate([omega_C, psi_C], axis=0)
-    omega_joint_non_causal = K_joint_non_causal @ jnp.concatenate([omega_C, psi_CE], axis=0)
-
-    ## Compute errors
-    non_causal_err_marginal = jnp.square(omega_marginal_non_causal - omega_C_t).mean()
-    non_causal_err_joint = jnp.square(omega_joint_non_causal[:N] - omega_C_t).mean()
-    non_causal_model_error = non_causal_err_joint - non_causal_err_marginal
+    # Compute K
+    K_marginal, K_joint = compute_K(omega_E, psi_E, omega_EC, psi_EC, omega_Et)
+    
+    # Compute marginal / joint
+    omega_marginal = K_marginal @ jnp.concatenate([omega_E, psi_E], axis=0)
+    omega_joint = K_joint @ jnp.concatenate([omega_E, psi_EC], axis=0)
+    
+    # Compute errors
+    causal_err_marginal = jnp.sqrt(jnp.mean(jnp.square(omega_marginal - omega_Et)))
+    causal_err_joint = jnp.sqrt(jnp.mean(jnp.square(omega_joint - omega_Et)))
+    causal_model_error = causal_err_marginal - causal_err_joint
 
     return (
         causal_model_error,
-        non_causal_model_error,
-        omega_marginal_causal,
-        omega_joint_causal,
-        omega_marginal_non_causal,
-        omega_joint_non_causal,
+        omega_marginal,
+        omega_joint
     )
