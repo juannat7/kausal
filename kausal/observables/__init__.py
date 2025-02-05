fdd1d6bf7cc30733d26a553bf066e35d7cee38f9
juannat7 <nathanieljuan@gmail.com>
2025-02-04 22:27:40 -0500

    add polynomial observable

--- kausal/observables/__init__.py
+++ kausal/observables/__init__.py
@@ -2,3 +2,4 @@ from .random_fourier_features import RandomFourierFeatures
 from .mlp_features import MLPFeatures
 from .cnn_features import CNNFeatures
 from .identity_features import IdentityFeatures
+from .polynomial_features import PolynomialFeatures
---
+++ kausal/observables/polynomial_features.py
@@ -0,0 +1,48 @@
+import torch
+import itertools
+from .base import BaseObservables
+
+class PolynomialFeatures(BaseObservables):
+    """
+    Returns features of polynomials
+
+    Parameters:
+        degree (int): 
+        interaction_only (bool):
+
+    Returns
+        Identity (torch.Tensor): The Identity transforms.
+    """
+
+    def __init__(
+        self,
+        degree = 2
+    ):
+        super().__init__()
+        self.degree = degree
+
+    def fit(self):
+        return self
+    
+    def forward(
+        self, 
+        X
+    ):
+        X = self.validate(X)
+        N, D = X.shape
+        
+        Psi = []
+        
+        # For each [1, degree + 1]
+        for d in range(1, self.degree + 1):
+
+            # Indices is a tuple e.g., (0, 2) meaning we multiply X[0] * X[2]
+            I = itertools.combinations_with_replacement(range(N), d)
+            
+            for i in I:
+                
+                Psi.append(
+                    torch.prod(X[list(i), :], dim=0, keepdim=True)
+                )
+        
+        return torch.cat(Psi, dim=0)
